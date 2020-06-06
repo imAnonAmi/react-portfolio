21d941e52e837bfb26b9e711e1ad8ff2ba856751
imAnonAmi <mcintireaw@gmail.com>
2020-06-06 09:54:07 -0700

    Cleaned up home page image, and optimized further. Basic structure of About imported, and will be used as template for other sections.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,32 +1,51 @@
 import React from "react";
 import { Link, Route } from "react-router-dom";
 import Learn from "./Learn";
+import myImage from "../../images/hs_AM.jpg";
 
 function About(props) {
   return (
-    <div class={{ container: 100 }} style={{padding: 20, paddingTop: 50 }}>
-      <h1>Actually the About Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-        Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-        pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-        tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec
-        metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula.
-        Nam tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora
-        torquent per conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-        Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-      <Link to="/learn" role="button" className="btn btn-link">
+<body>  
+<div class={{ container: '100%' }} style={{padding: 20, paddingTop: 20 }}>
+<div class="container" style={{marginBottom: 20, padding: 30}} width="100%">
+
+<h2 style={{color: 'darkgrey'}}>About Me</h2>
+
+<div class="row">
+  
+  <section>
+  <p style={{padding: 10}}>
+  <img src={myImage} class="rounded" width= "300px" height="300px" alt="Andrew McIntire: Full stack developer and senior operations leader" style={{float: 'left', marginRight: 40}} />
+  
+  Strategic, multi-disciplinary business operations leader and full stack developer with a proven aptitude for delivering high-growth results, exceeding operational performance targets, and yielding measurable outcomes while managing an extensive scope of responsibility. </p>
+
+<ul  style={{padding: 10, marginLeft: 20}}>
+
+<li>Expertly builds and optimizes organizational processes, measurement systems, and infrastructure to maximize business results across diverse operations, including publishing, aerospace, and retail/ecommerce industries</li>
+<li>Transforms strategic plans into actionable solutions and benchmarks performance against key operational targets and goals</li>
+<li>Establishes open and transparent relationships with colleagues, clients, and business partners to organically bridge the gaps between disparate groups, cultures, concepts, perspectives, and expectations</li>
+<li>Leverages multi-disciplinary background to identify and cultivate connections that strengthen company’s competitive advantage</li>
+</ul>
+
+</section>
+ 
+
+</div>
+<Link to="/learn" role="button" className="btn btn-link">
         Learn More
       </Link>
       
       <Route exact path={`${props.match.url}/learn`} component={Learn} />
+</div>
+					
+			
+      
       <div id="footer">
 	<p id="name">Andrew McIntire</p>
 	<p id="copyright">&copy; 2020</p>
 </div>
     </div>
+    </body>    
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,24 +1,37 @@
 import React from "react";
+import Background from '../../images/smith.jpg';
+
 
 function Home() {
   return (
-    <div class={{ container: 100 }} style={{padding: 20, paddingTop: 50 }}>
-      <h1>Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque velit, lobortis ut magna
-        varius, blandit rhoncus sem. Morbi lacinia nisi ac dui fermentum, sed luctus urna tincidunt.
-        Etiam ut feugiat ex. Cras non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna
-        imperdiet ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras rutrum
-        ligula in tincidunt commodo. Morbi sit amet mollis orci, in tristique ex. Donec nec ornare
-        elit. Donec blandit est sed risus feugiat porttitor. Vestibulum molestie hendrerit massa non
-        consequat. Vestibulum vitae lorem tortor. In elementum ultricies tempus. Interdum et
-        malesuada fames ac ante ipsum primis in faucibus.
-      </p>
+    
+    <div class={{ container: 100 }} style={{padding: 0, paddingTop: 0 }}>
+    <img className="backgroundImage" src={Background} alt={"Error"} width="100%"/>
+    <div class="centered" style={{
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+    
+      
+      <h1 style={{color: 'white', textAlign: 'center'}}>  Hi! My name is Andrew.</h1>
+      
+      <h2 style={{color: 'white', textAlign: 'center'}}>
+        This is my happy place.
+      </h2>
+      <br></br>
+      <h3 style={{color: 'white', textAlign: 'center'}}>
+        It inspires my work and play.
+      </h3>
+      
+      </div>  
+      
       <div id="footer">
 	<p id="name">Andrew McIntire</p>
 	<p id="copyright">&copy; 2020</p>
 </div>
     </div>
+    
+   
   );
 }
 
